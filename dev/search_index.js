9e70a3ceff7a9e4839e5f8546581c1a0bbad1814
Documenter.jl <documenter@juliadocs.github.io>
2025-08-19 07:14:54 +0000

    build based on e1c853e

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Knight","category":"page"},{"location":"#Knight","page":"Home","title":"Knight","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Knight.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Knight]","category":"page"}]
+[{"location":"#Knight","page":"Home","title":"Knight","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Knight.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
 }
